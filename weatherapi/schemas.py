1c9bfcc864a005496085ad09efed08f3a49bd546
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2021-11-16 21:57:46 -0800

    chore: pydantic models

--- weatherapi/schemas.py
+++ weatherapi/schemas.py
@@ -1,10 +1,22 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
+# defines the  schema to be used
 class ErrorResponse(BaseModel):
     code: str
     reason: str
     message: str
-    schemaLocation: Optional[str]
+
+
+class WeatherObject(BaseModel):
+    maximum: int
+    minimum: int
+    average: int
+    median: int
+
+
+class WeatherRequestParams(BaseModel):
+    days: int = Field(1)
+    city: str
--- weatherapi/views.py
+++ weatherapi/views.py
@@ -1,7 +1,11 @@
-from django.http import  JsonResponse
-
+from django.http import JsonResponse
 
 # Create your views here.
+from weatherapi.schemas import WeatherRequestParams
+
+
 def weather_view(request, city):
-    days = int(request.GET.get('days', 1))
-    return JsonResponse({"greetings":city})
+    request_params: WeatherRequestParams = WeatherRequestParams(city=city,
+                                                                **request.GET.dict())
+
+    return JsonResponse({"greetings": request_params.dict()})
