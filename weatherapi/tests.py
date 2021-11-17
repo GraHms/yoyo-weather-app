18de756ae966c0acede78cf27a60a35a01c452ee
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2021-11-17 03:46:33 -0800

    fix: cognitive complexity

--- weatherapi/tests.py
+++ weatherapi/tests.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
-
-# Create your tests here.
-from rest_framework.exceptions import APIException
-from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
-class ServiceUnavailable(APIException):
-    status_code = 503
-    default_detail = 'Service temporarily unavailable, try again later.'
-    default_code = 'service_unavailable'
+class WeatherTests(APITestCase):
+    def test_method_not_allowed(self):
+        response = self.client.post('/api/locations/maputo')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_resource_not_found(self):
+        response = self.client.post('/api/locations/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- weatherapi/views.py
+++ weatherapi/views.py
@@ -1,7 +1,7 @@
-import logging
+# This file includes the weather business logic and view serialization
 
+import logging
 import requests
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, StaticHTMLRenderer
 from rest_framework.response import Response
@@ -12,6 +12,30 @@ from weatherapi.serializers import WeatherSerializer
 from yoyoweatherapp.settings import WEATHER_API_KEY
 
 
+def compute_temperatures(weatherlist):
+
+    # we take the first maximum as result
+    max_temp = weatherlist['day'][0]["maxtemp_c"]
+    # we take the first days avg temperature as defaull
+    min_temp = weatherlist['day'][0]["mintemp_c"]
+    all_avg_temp = []
+    for res in weatherlist:
+        _max_temp = res['day']["maxtemp_c"]
+        _min_temp = res['day']["mintemp_c"]
+
+        all_avg_temp.append(res['day']['avgtemp_c'])
+
+        # we compare all other days with the first to find the maximum
+        if _max_temp > max_temp:
+            # replace if finds a new maximum
+            max_temp = _max_temp
+        # we compare all other days with the first to find the minimum
+        if _min_temp < min_temp:
+            # replace if finds a new minimum
+            min_temp = _min_temp
+        return max_temp, min_temp, all_avg_temp
+
+
 def get_weather(input_values: WeatherRequestParams):
     weatherapi_basepath = "http://api.weatherapi.com/v1/forecast.json"
 
@@ -19,46 +43,41 @@ def get_weather(input_values: WeatherRequestParams):
         params = {"key": WEATHER_API_KEY,
                   "q": input_values.city,
                   "days": input_values.days}
+
         result = requests.get(weatherapi_basepath, params=params)
         if result.status_code == 200:
             weatherlist = result.json().get('forecast')['forecastday']
-            max_temp = 0
-            all_avg_temp = []
-            min_temp = None
+            max_temp, min_temp, all_avg_temp = compute_temperatures(weatherlist)
             days = len(weatherlist)
-            for res in weatherlist:
-                _max_temp = res['day']["maxtemp_c"]
-                _min_temp = res['day']["mintemp_c"]
-
-                all_avg_temp.append(res['day']['avgtemp_c'])
-
-                if _max_temp > max_temp:
-                    max_temp = _max_temp
-                if min_temp is None:
-                    min_temp = _min_temp
-                else:
-                    if _min_temp < min_temp:
-                        min_temp = _min_temp
-            # calculates median
             median_temp = (max_temp + min_temp) / 2
             # Average temperature = Sum of temperatures of all the days / *no. of days
             avg_temp = sum(all_avg_temp) / days
             data = {"minimum": min_temp, "maximum": max_temp, "average": round(avg_temp, 2),
                     "median": round(median_temp, 2)}
-            return data
+            return data, result.status_code
+        else:
+            # dynamically maps error messages and status code from weatherapi
+            logging.exception(result)
+            return {"message": result.json().get('error')['message']}, result.status_code
+
     except Exception as err:
+        # in case of a network exception or something out of our api control
         logging.exception(err)
-        JsonResponse({"message": err}, status=500)
+        Response({"message": err}, status=500)
 
 
 @api_view(['GET'])
 @renderer_classes([JSONRenderer, StaticHTMLRenderer])
 def weather_view(request, city):
+    # request values
     input_values: WeatherRequestParams = WeatherRequestParams(city=city,
                                                               **request.GET.dict())
+    # api results
+    result, status_code = get_weather(input_values=input_values)
 
-    weather_data = get_weather(input_values=input_values)
-    result: WeatherSerializer = WeatherSerializer(data=weather_data)
-    result.is_valid(raise_exception=True)
-    logging.log(result.data)
-    return Response(result.data)
+    if status_code == 200:
+        result: WeatherSerializer = WeatherSerializer(data=result)
+        result.is_valid(raise_exception=True)
+        result = result.data
+
+    return Response(result, status=status_code)
