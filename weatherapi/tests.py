0065978b396d84ec6080d544f7c201a611e827c6
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2021-11-17 04:12:59 -0800

    fix: bug fixes

--- weatherapi/tests.py
+++ weatherapi/tests.py
@@ -1,6 +1,9 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from weatherapi.schemas import WeatherRequestParams
+from weatherapi.views import get_weather
+
 
 class WeatherTests(APITestCase):
     def test_method_not_allowed(self):
@@ -16,6 +19,15 @@ class WeatherTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json().get("message"), "No matching location found.")
 
-    def test_get_weather(self):
+    def test_get_weather_api(self):
+        input_values: WeatherRequestParams = WeatherRequestParams(city='maputo',
+                                                                  days=2)
+
+        result, status_code = get_weather(input_values=input_values)
         response = self.client.get('/api/locations/maputo?days=2')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), result)
+        self.assertEqual(response.status_code, status_code)
+
+
+
